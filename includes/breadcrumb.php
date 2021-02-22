e0395351233e0fe345b33c42d116656f9758519c
emil_tamayo <zen4emil@gmail.com>
2021-02-22 15:29:58 +0800

    may mali po sa assets sa table

--- includes/breadcrumb.php
+++ includes/breadcrumb.php
@@ -1,5 +1,5 @@
 <ol class="breadcrumb float-xl-right">
-                <li class="breadcrumb-item"><a href="javascript:;">Home</a></li>
-                <li class="breadcrumb-item active">Dashboard</li>
-            </ol>
-            <h1 class="page-header">Dashboard <small>header small text goes here...</small></h1>
+    <li class="breadcrumb-item"><a href="javascript:;">Home</a></li>
+    <li class="breadcrumb-item active">Dashboard</li>
+</ol>
+<h1 class="page-header">Dashboard <small>header small text goes here...</small></h1>
