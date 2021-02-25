d3270d1c86ce2f50adc85bb4bb72e86dd234d313
emil_tamayo <zen4emil@gmail.com>
2021-02-25 15:23:21 +0800

    Update breadcrumb.php
    if empty $page_header it will show default header

--- includes/breadcrumb.php
+++ includes/breadcrumb.php
@@ -1,8 +1,5 @@
-<?php
-$page_header = "Dashboard <small>header small text goes here...</small>";
-?>
    <ol class="breadcrumb float-xl-right">
     <li class="breadcrumb-item"><a href="javascript:;">Home</a></li>
     <li class="breadcrumb-item active">Dashboard</li>
 </ol>
-<h1 class="page-header"><?php echo $page_header; ?></h1>
+<h1 class="page-header"><?php echo ($page_header) ? $page_header : "Dashboard <small>header small text goes here...</small>"; ?></h1>
