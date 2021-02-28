7e69e533f2e02fe5c36970062c07fb62d8f62e78
emil_tamayo <zen4emil@gmail.com>
2021-02-28 22:14:28 +0800

    Updated the Gritter Notifications

--- includes/footer.php
+++ includes/footer.php
@@ -26,12 +26,22 @@
 <script src="assets/plugins/bootstrap-datepicker/dist/js/bootstrap-datepicker.js"></script>
 <script src="assets/js/demo/dashboard.js"></script>
 <script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="defaa6b258bf360c301851d1-|49" defer=""></script>
-
-
-
 <script src="assets/plugins/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="assets/plugins/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
 <script src="assets/plugins/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
 <script src="assets/plugins/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
 <script src="assets/plugins/highlight.js/highlight.min.js"></script>
 <script src="assets/js/demo/render.highlight.js"></script>
+<script>
+    setTimeout(function() {
+        $.gritter.add({
+            title: 'Welcome back, <?php echo $sess; ?>!',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus lacus ut lectus rutrum placerat.',
+            image: 'assets/img/user/user-2.jpg',
+            sticky: true,
+            time: '',
+            class_name: 'my-sticky-class'
+        });
+    }, 1000);
+
+</script>
